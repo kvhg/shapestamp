f59daeff8621ea22faba3cf24aa3c94da4bd5722
kvhg <kavehg@gmail.com>
2026-03-27 16:51:05 -0700

    Added Saved/Not indicator

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shapestamp-v2';
+const CACHE = 'shapestamp-v3';
 const ASSETS = ['./shapestamp.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
